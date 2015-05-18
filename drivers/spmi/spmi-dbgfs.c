859504efccf380510e5ae61dbf8bcc3aaf87c82a
scoty755 <scoty755@gmail.com>
2015-05-19 00:09:19 +0900

    spmi(System Power Management Interface): update the driver for B2

--- drivers/spmi/spmi-dbgfs.c
+++ drivers/spmi/spmi-dbgfs.c
@@ -1238,16 +1238,34 @@ int htc_vreg_dump(int vreg_id, struct seq_file *m, char *vreg_buffer, int curr_l
 
         return curr_len;
 }
-#if defined(CONFIG_MACH_EYE_UL)
+#if defined(CONFIG_MACH_B2_UL)
+#define PN547_I2C_POWEROFF_SEQUENCE_FOR_B2
+#elif defined(CONFIG_MACH_EYE_UL)
 #define PN547_I2C_POWEROFF_SEQUENCE_FOR_EYE
 #elif defined(CONFIG_MACH_EYE_WL)
 #define PN547_I2C_POWEROFF_SEQUENCE_FOR_EYE
 #elif defined(CONFIG_MACH_EYE_WHL)
 #define PN547_I2C_POWEROFF_SEQUENCE_FOR_EYE
-#elif defined(CONFIG_MACH_B2_UL)
-#define PN547_I2C_POWEROFF_SEQUENCE_FOR_B2
+#else
 #endif
-#if defined(PN547_I2C_POWEROFF_SEQUENCE_FOR_EYE)
+#if defined(PN547_I2C_POWEROFF_SEQUENCE_FOR_B2)
+void force_disable_PMICGPIO34(void)
+{
+	int ret;
+	uint8_t voltage_sel = 0x10;
+
+	ret = spmi_write_data(qpnp_vregs.ctrl, &voltage_sel, 0xE140, 1);
+
+}
+void force_disable_PMICLVS1(void)
+{
+	int ret;
+	uint8_t voltage_sel = 0x0;
+
+	ret = spmi_write_data(qpnp_vregs.ctrl, &voltage_sel, 0x18046, 1);
+
+}
+#elif defined(PN547_I2C_POWEROFF_SEQUENCE_FOR_EYE)
 void force_disable_PM8941_VREG_ID_L22(void)
 {
 	int ret;
@@ -1259,6 +1277,7 @@ void force_disable_PM8941_VREG_ID_L22(void)
                 pr_err("force_disable_PM8941_VREG_ID_L22, SPMI write failed, err = %zu\n", ret);
         }
 }
+#else
 #endif
 
 int htc_vregs_dump(char *vreg_buffer, int curr_len)
